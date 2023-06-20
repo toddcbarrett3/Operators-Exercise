28d8750e66660e4eb5ed8fca52ad4d012e4b0061
Todd Barrett <toddcbarrett3@gmail.com>
2023-06-20 11:45:47 -0700

    Completed Exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1:
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if(a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+
+
         }
     }
 }
